5bea36ab961fe3ec8c0d8258a7a83f71bc16de3d
Ashutosh Parihar <ashutoshparihar0@gmail.com>
2023-03-04 09:22:41 +0000

    tf.3

--- DockerTf/main.tf
+++ DockerTf/main.tf
@@ -9,25 +9,25 @@ terraform {
 provider "docker" {
   
 }
-resource "random_string" "name" {
-  count = 2
-  length = 4
-  special = false
-  upper = false
-}
+# resource "random_string" "name" {
+#   count = 2
+#   length = 4
+#   special = false
+#   upper = false
+# }
 resource "docker_image" "node" {
     name="nodered/node-red:latest"
   
 }
-resource "docker_container" "nodecontainer" {
-  count = 2
-  name=join("-",["nodecontainer",random_string.name[count.index].result])
-  image=docker_image.node.latest
-  ports{
-    # external=1880
-    internal=1880
-  }
-}
+# resource "docker_container" "nodecontainer" {
+#   count = 2
+#   name=join("-",["nodecontainer",random_string.name[count.index].result])
+#   image=docker_image.node.latest
+#   ports{
+#     # external=1880
+#     internal=1880
+#   }
+# }
 # output "Ip" {
 #   value = docker_container.nodecontainer.ip_address
 #   description = "tis is cintainer ip"
